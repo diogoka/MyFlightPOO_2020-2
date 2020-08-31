99d49288c9494942a3e9d4e0ccc6de4839a0db2e
Diogo <diogo.krub@edu.pucrs.br>
2020-08-31 19:02:21 -0300

    Continuação da implementação das classes base.

--- GerenciadorAeronaves.java
+++ GerenciadorAeronaves.java
@@ -1,3 +1,41 @@
+import java.util.ArrayList;
+
 public class GerenciadorAeronaves {
+   
+    private ArrayList<Aeronave> Aeronaves;
+
+    public static GerenciadorAeronaves instance;
+
+    private GerenciadorAeronaves (){
+        Aeronaves = new ArrayList<>();
+    }
+
+    public static GerenciadorAeronaves getInstance(){
+        if (instance == null){
+            instance = new GerenciadorAeronaves();
+        }
+        return instance;
+    }
+
+    public void inserir(Aeronave umaAeronave){
+        Aeronaves.add(umaAeronave);
+    }
+
+    public Aeronave pesquisar(String umCodigo){
+        for (Aeronave Aero : Aeronaves){
+            if (Aero.getCodigo() == umCodigo){
+                return Aero;
+            }
+        }
+        return null;
+    }
+
+    public void listar(){
+        System.out.println("Lista de Aeronaves");
+        for (Aeronave Aero : Aeronaves){
+            System.out.println(Aero.toString());
+        }
+    }
+
     
 }
--- GerenciadorCias.java
+++ GerenciadorCias.java
@@ -1,3 +1,43 @@
+import java.util.ArrayList;
+
 public class GerenciadorCias {
+
+    private ArrayList<CiaAerea> Companhias;
+
+    private static GerenciadorCias instance;
+
+    private GerenciadorCias(){
+        Companhias = new ArrayList<>();
+    }
+
+    public static GerenciadorCias getInstance(){
+        if (instance == null){
+            instance = new GerenciadorCias();
+        }
+        return instance;
+    }
+
+    public void inserir(CiaAerea umaCiaAerea){
+        Companhias.add(umaCiaAerea);
+    }
+
+    public CiaAerea pesquisar (String umCodigo){
+        for(CiaAerea CIA : Companhias){
+            if(CIA.getCodigo() == umCodigo){
+                return CIA;
+            }                
+        }
+        return null;
+    }
+
+    public void listar(){
+        System.out.println("Cadastro das Companhias Aereas");
+        for (CiaAerea CIA : Companhias){
+            System.out.println(CIA.toString());
+        }
+
+    }
+
+
     
 }
