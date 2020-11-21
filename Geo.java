9a7c8d417909eb610dd466f02766fb52ecd95ab2
Douglas Sobreira <dodosobreira@gmail.com>
2020-11-21 12:14:27 -0300

    Adicionando metodo na classe GEO para calcular distancia

--- Geo.java
+++ Geo.java
@@ -15,6 +15,18 @@ public Geo (double umaLatitude, double umaLongitude){
         return longitude;
     }
 
+    public static double calcular(Geo obj, Geo obj2) {
+        double earthR = 6372.8;
+        double dLat = Math.toRadians(obj2.latitude - obj.latitude);
+        double dLon = Math.toRadians(obj2.longitude - obj.longitude);
+        obj.latitude = Math.toRadians(obj.latitude);
+        obj2.latitude = Math.toRadians(obj2.latitude);
+
+        double a = Math.pow(Math.sin(dLat / 2),2) + Math.pow(Math.sin(dLon / 2),2) * Math.cos(obj.latitude) * Math.cos(obj2.latitude);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        return earthR * c;
+    }
+
        public String toString(){
         return "Dados da Geo Localização: \n Latitude: \n" + latitude + "Longitude: " + longitude;
     }
